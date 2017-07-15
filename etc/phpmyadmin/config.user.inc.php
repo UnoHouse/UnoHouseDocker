5b4ff3ee4edfe05ff25127ba15068461928f7a59
Rafal Salwa <rafal.salwa@mamf.pl>
2017-07-16 00:57:11 +0200

    feat(travis,pma): added phpmyadmin and travis tests

---
+++ etc/phpmyadmin/config.user.inc.php
@@ -0,0 +1,8 @@
+$cfg['Servers'][$i]['AllowNoPassword'] = TRUE;
+$cfg['Servers'][1]['auth_type'] = 'config';
+<!--$cfg['Servers'][1]['host'] = 'localhost'; //edit if you have db in the other host-->
+$cfg['Servers'][1]['connect_type'] = 'tcp';
+$cfg['Servers'][1]['compress'] = false;
+$cfg['Servers'][1]['extension'] = 'mysql';
+$cfg['Servers'][1]['user'] = 'root'; //edit this line
+$cfg['Servers'][1]['password'] = 'password';
