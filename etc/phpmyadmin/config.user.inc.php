91f1b1d9669357682c68a409403eb0014921fc31
Rafal Salwa <rafal.salwa@mamf.pl>
2017-07-16 18:37:59 +0200

    feat(gitingore): new restrictions

--- etc/phpmyadmin/config.user.inc.php
+++ etc/phpmyadmin/config.user.inc.php
@@ -1,8 +1,8 @@
 $cfg['Servers'][$i]['AllowNoPassword'] = TRUE;
-$cfg['Servers'][1]['auth_type'] = 'config';
-<!--$cfg['Servers'][1]['host'] = 'localhost'; //edit if you have db in the other host-->
+$cfg['Servers'][1]['auth_type'] = 'cookie';
+$cfg['Servers'][1]['host'] = 'unohouse.dev'; //edit if you have db in the other host
 $cfg['Servers'][1]['connect_type'] = 'tcp';
 $cfg['Servers'][1]['compress'] = false;
 $cfg['Servers'][1]['extension'] = 'mysql';
 $cfg['Servers'][1]['user'] = 'root'; //edit this line
-$cfg['Servers'][1]['password'] = 'password';
+$cfg['Servers'][1]['password'] = 'unohouse';
