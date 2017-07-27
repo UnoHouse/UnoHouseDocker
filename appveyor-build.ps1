89fe2be69102914e1a7099c44127ad14689d27bf
Rafal Salwa <pirat1690@gmail.com>
2017-07-27 20:32:08 +0200

    fix(appveyor.yml): trying to get appveyor working

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -8,13 +8,13 @@ docker pull microsoft/nanoserver
 docker version
 Write-Host Updating Docker engine to master
 #Stop-Service docker
+Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
 $version = "17.05.0-ce"
 $wc = New-Object net.webclient
 $wc.DownloadFile("https://get.docker.com/builds/Windows/x86_64/docker-$version.zip", "$env:TEMP\docker.zip")
 Expand-Archive -Path "$env:TEMP\docker.zip" -DestinationPath $env:ProgramFiles -Force
 Remove-Item "$env:TEMP\docker.zip"
 Start-Service docker
-Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
 docker version
 
 docker-compose build
