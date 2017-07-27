f61ead4727d47fc1279ef303101a58b8bd01072d
Rafal Salwa <pirat1690@gmail.com>
2017-07-27 21:10:29 +0200

    fix(appveyor.yml): trying to get appveyor working

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -6,7 +6,7 @@ choco install -y docker-compose -version 1.15.0
 docker version
 docker-compose version
 
-docker-compose build
-docker-compose up -d
+#docker-compose build
+docker-compose up
 docker images
 docker ps
