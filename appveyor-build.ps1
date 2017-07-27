c5a60d83e28b004ae0c875aaf3a79d2500462834
Rafal Salwa <pirat1690@gmail.com>
2017-07-27 20:35:34 +0200

    fix(appveyor.yml): trying to get appveyor working

--- appveyor-build.ps1
+++ appveyor-build.ps1
@@ -9,13 +9,13 @@ docker version
 Write-Host Updating Docker engine to master
 #Stop-Service docker
 Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
-$version = "17.05.0-ce"
-$wc = New-Object net.webclient
-$wc.DownloadFile("https://get.docker.com/builds/Windows/x86_64/docker-$version.zip", "$env:TEMP\docker.zip")
-Expand-Archive -Path "$env:TEMP\docker.zip" -DestinationPath $env:ProgramFiles -Force
-Remove-Item "$env:TEMP\docker.zip"
-Start-Service docker
-docker version
+#$version = "17.05.0-ce"
+#$wc = New-Object net.webclient
+#$wc.DownloadFile("https://get.docker.com/builds/Windows/x86_64/docker-$version.zip", "$env:TEMP\docker.zip")
+#Expand-Archive -Path "$env:TEMP\docker.zip" -DestinationPath $env:ProgramFiles -Force
+#Remove-Item "$env:TEMP\docker.zip"
+#Start-Service docker
+#docker version
 
 docker-compose build
 
